75b900bebe35750311c4fce162c08086e904b17f
AoungY <2377289135@qq.com>
2023-06-05 17:35:52 +0800

    去除debugger

--- api/proxy.js
+++ api/proxy.js
@@ -2,7 +2,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = (req, res) => {
   let target = ''
-    debugger
   // 代理目标地址
   // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
   if (req.url.startsWith('/chatGPT')) {
