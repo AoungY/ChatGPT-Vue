67ded3cb0058b124aa5bfbd5f1d018807727450f
AoungY <2377289135@qq.com>
2023-06-05 17:46:27 +0800

    修改成es模块

--- api/proxy.js
+++ api/proxy.js
@@ -1,5 +1,5 @@
 import { createProxyMiddleware } from 'http-proxy-middleware';
-module.exports = (req, res) => {
+export default function handler(req, res) {
   let target = ''
   // 代理目标地址
   // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
